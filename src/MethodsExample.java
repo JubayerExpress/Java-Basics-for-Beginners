05bcd06f2b45a68f4308a7fd4b6d707d4e11677d
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-22 23:49:12 +0600

    Create MethodsExample.java

---
+++ src/MethodsExample.java
@@ -0,0 +1,10 @@
+public class MethodsExample {
+    public static void main(String[] args) {
+        int result = addNumbers(10, 20);
+        System.out.println("Sum: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
