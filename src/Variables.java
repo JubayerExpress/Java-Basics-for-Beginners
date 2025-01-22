1151e8dba0992040e88d4fb478a1441ff41dabdf
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-22 23:48:09 +0600

    Create Variables.java

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 50000.50;
+        boolean isEmployed = true;
+        char grade = 'A';
+        String name = "John Doe";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Employed: " + isEmployed);
+        System.out.println("Grade: " + grade);
+    }
+}
